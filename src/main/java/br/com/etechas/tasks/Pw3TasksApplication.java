8a28cae059d0bb858e0c2163d2504d9719f6ccf5
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2025-07-29 09:26:01 -0300

    project uploaded

---
+++ src/main/java/br/com/etechas/tasks/Pw3TasksApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechas.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw3TasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3TasksApplication.class, args);
+	}
+
+}
