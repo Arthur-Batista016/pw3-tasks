7dc0e58319ee1a5f41f3b966bc67f6b004615cd0
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2025-08-26 11:07:30 -0300

    Feat: delete mapping added

--- src/main/java/br/com/etechas/tasks/controllers/TaskController.java
+++ src/main/java/br/com/etechas/tasks/controllers/TaskController.java
@@ -3,17 +3,20 @@ package br.com.etechas.tasks.controllers;
 
 import br.com.etechas.tasks.dto.TaskResponseDTO;
 import br.com.etechas.tasks.entity.Task;
+import br.com.etechas.tasks.entity.enums.StatusEnum;
 import br.com.etechas.tasks.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/task")
 public class TaskController {
+    //Arthur Bonvecchio Batista
+    //Gabriel Whanderson Silva de Castro
 
     @Autowired
     private TaskService service;
@@ -24,6 +27,21 @@ public class TaskController {
     public List<TaskResponseDTO> listTasks(){
         List<TaskResponseDTO> result = service.findTasks();
         return result;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> DeleteById(@PathVariable Long id){
+        var  existe = service.findTaskById(id);
+        if(existe.isPresent()){
+            if(existe.get().getStatus() == StatusEnum.PENDING) {
+                service.DeleteListById(id);
+                return ResponseEntity.noContent().build();
+            }else {
+                throw new RuntimeException("Tarefa em Andamento");
+            }
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
--- src/main/java/br/com/etechas/tasks/services/TaskService.java
+++ src/main/java/br/com/etechas/tasks/services/TaskService.java
@@ -2,15 +2,24 @@ package br.com.etechas.tasks.services;
 
 import br.com.etechas.tasks.dto.TaskResponseDTO;
 import br.com.etechas.tasks.entity.Task;
+import br.com.etechas.tasks.entity.enums.StatusEnum;
 import br.com.etechas.tasks.mapper.TaskMapper;
 import br.com.etechas.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityReturnValueHandler;
 
+import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
+    //Arthur Bonvecchio Batista
+    //Gabriel Whanderson Silva de Castro
+
 
     @Autowired
     private TaskRepository repository;
@@ -18,10 +27,21 @@ public class TaskService {
     @Autowired
     private TaskMapper taskMapper;
 
+
     //alterar para retornar euma lista de DTO
     public List<TaskResponseDTO> findTasks(){
         List<TaskResponseDTO> result = taskMapper.toResponseDTOList(repository.findAll());
         return result;
+    }
+
+    public Optional<Task> findTaskById(Long id){
+        return repository.findById(id);
+
+    }
+
+    public void DeleteListById(Long id) {
+
+        repository.deleteById(id);
     }
 
 
